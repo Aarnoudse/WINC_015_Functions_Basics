704c4da59ab682f3b8c661a7f1e46bd46893e47c
Aarnoudse <83244265+Aarnoudse@users.noreply.github.com>
2021-06-06 13:30:36 +0200

    Add files via upload

---
+++ WINC_015_Functions_Basics/script.js
@@ -0,0 +1,29 @@
+const makePizza = function () {
+    console.log("Make dough");
+    console.log("Flaten dough");
+    console.log("Put ingredients on dough");
+    console.log("Bake 20 min 200 degrees");
+}
+
+/*
+makePizza();
+makePizza();
+makePizza();
+*/
+
+const makeSushi = function () {
+    console.log("Get rice");
+    console.log("Put fish on the rice");
+    console.log("Roll it");
+    console.log("Cut it");
+}
+
+/*  
+makeSushi();
+*/
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
